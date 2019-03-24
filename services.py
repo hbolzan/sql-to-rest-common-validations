cb2509b34d9acb9ef1cbd03590e2c0b75ac623ef
Henrique Bolzan <henrique@bolzan.com.br>
2019-03-24 12:11:45 -0300

    Add health check

--- services.py
+++ services.py
@@ -1,6 +1,7 @@
 import requests
 from nameko.rpc import rpc
 from dstr_common_lib import responses
+from dstr_common_lib.common_mixin import CommonMixin
 from dstr_common_lib.util import clear_punctuation
 from dstr_common_lib.consts import HTTP_STATUS_OK, HTTP_STATUS_ERROR
 from logic import cnpj, cpf
@@ -13,8 +14,12 @@ VALIDATIONS = {
     DOC_TYPE_CNPJ: cnpj,
 }
 
-class CommonValidationsService:
+class CommonValidationsService(CommonMixin):
     name = "common_validations"
+
+    @rpc
+    def health_check(self):
+        return self.common_health_check()
 
     @rpc
     def cpf(self, cpf_number):
@@ -35,6 +40,8 @@ class CommonValidationsService:
         r = requests.get("https://viacep.com.br/ws/{}/json/".format(cep_number))
         # TODO: fallback to local cache
         if r.status_code == 200:
+            if r.json().get("erro"):
+                return self.error("CEP", cep_number, None, 409)
             return self.success("CEP", cep_number, r.json())
         return self.error("CEP", cep_number, None, r.status_code)
 
