52f3f7d52bac8bd5a248331a4b9c9d11eca03de0
Henrique Bolzan <henrique@bolzan.com.br>
2019-04-18 18:54:30 -0300

    Nameko + RabbitMQ working

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,4 +5,5 @@ until nc -z ${RABBIT_HOST} ${RABBIT_PORT}; do
     sleep 1
 done
 
+echo "AMQP_URI: amqp://${RABBIT_USER:-rabbitmq}:${RABBIT_PASSWORD:-rabbitmq}@${RABBIT_HOST:-nameko-rabbitmq}:${RABBIT_PORT:-5672}/" > config.yml
 nameko run --config config.yml services
